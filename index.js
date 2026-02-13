9878c41b1412029512fa834fa7d6292c90990b0c
Savyez <visnuk252@gmail.com>
2026-02-14 01:02:18 +0530

    Add POST route and Prevent dupalication of Visited countries

--- index.js
+++ index.js
@@ -17,12 +17,19 @@ const db = new pg.Client({
 });
 db.connect();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+async function isCountryVisited(countryCode) {
+  const result = await db.query(
+    "SELECT country_code FROM visited_country WHERE country_code = $1",
+    [countryCode]
+  );
+  return result.rows.length > 0;
+}
 
+// HOME ROUTE
 app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT country_code FROM visited_country");
@@ -41,7 +48,39 @@ app.get("/", async (req, res) => {
 
 
 
+// POST ROUTE
+app.post("/add", async (req, res) => {
+  try {
+      const userCountry = req.body.country.trim();
+      const result = await db.query(
+        "SELECT country_code FROM countries WHERE LOWER(country_name) = LOWER($1)",
+        [userCountry]
+        );
+      if (result.rows.length === 0) {
+        return res.redirect("/");
+      }
 
+      const userCountryCode = result.rows[0].country_code;
+      const alreadyVisited = await isCountryVisited(userCountryCode);
+
+      if (!alreadyVisited) {
+        await db.query(
+          "INSERT INTO visited_country (country_code) VALUES ($1)",
+          [userCountryCode]
+        );
+      }
+      return res.redirect("/");
+
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).send("Database error");
+  }
+  db.end();
+});
+
+
+
+// LISTEN TO SERVER
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
